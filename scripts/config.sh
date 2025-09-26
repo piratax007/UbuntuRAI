671656be189ab3d342ad08fe411aaec3ea25fcb4
Fausto M. Lagos S <piratax007@protonmail.ch>
2025-09-26 17:10:20 +0200

    Improved typos

--- scripts/config.sh
+++ scripts/config.sh
@@ -93,7 +93,7 @@ function customize_image() {
 
     apt-get install -y --no-install-recommends linux-firmware network-manager
 
-    apt-get install -y shim-signed grup-efi-amd64-signed mokutil sbsigntool
+    apt-get install -y shim-signed grub-efi-amd64-signed mokutil sbsigntool
 
     if apt-cache show linux-generic >/dev/null 2>&1; then
         apt-get install -y linux-generic
